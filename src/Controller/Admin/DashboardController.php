7796a9381bdaff66a5159c9c646d97cdecb4656f
Vladimir Azarko <alu8bit@gmail.com>
2021-04-22 19:16:16 +0300

    Add EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        return $this->redirect($routeBuilder->setController(ConferenceCrudController::class)->generateUrl());
+
+        return  parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Guestbook');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Conference', 'fa fa-list', Conference::class)->setController(ConferenceCrudController::class);
+        yield MenuItem::linkToCrud('Comments', 'fa fa-list', Comment::class)->setController(CommentCrudController::class);
+    }
+}
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
         $greet = '';
-        if ($name) {
+        if ($name = $request->query->get('name')) {
             $greet = sprintf('<h1>Hello, %s</h1>', htmlspecialchars($name));
         }
         return new Response(<<<EOF
