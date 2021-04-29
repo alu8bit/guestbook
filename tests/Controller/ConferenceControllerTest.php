068488bda13f0fe48c4579fc971dc35693d3eff7
Vladimir Azarko <alu8bit@gmail.com>
2021-04-29 18:18:10 +0300

    Add conference check test

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -12,6 +12,17 @@ class ConferenceControllerTest extends WebTestCase
         $client->request('GET', '/');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
-//        $this->assertTrue(true);
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertCount(2, $crawler->filter('h4'));
+        $client->click($crawler->filter('h4 + p a')->link());
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
